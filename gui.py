41e8847a8a1cd0fbd999358334a550c2cccf1a6a
ThaumielSparrow <lzhang1337@gmail.com>
2022-08-29 16:15:01 -0400

    update gui

--- gui.py
+++ gui.py
@@ -1,17 +1,25 @@
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
 import sys
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.current_file = None
 
         self.setWindowTitle("OLEDpaper")
         load_button = QPushButton("Load Image")
         load_button.setCheckable(True)
-        load_button.clicked.connect()
+        load_button.clicked.connect(self.file_explorer)
 
         self.setCentralWidget(load_button)
+    
+    def file_explorer(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"Select Image", "","All Files (*);;PNG Image (*.png);;JPG Image (*.jpg)", options=options)
+        if fileName:
+            self.current_file = fileName
 
 app = QApplication(sys.argv)
 window = MainWindow()
