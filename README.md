114b92d25c7179b2a16e860b53fd6907e310eda4
ThaumielSparrow <lzhang1337@gmail.com>
2022-09-01 13:18:08 -0400

    stage to latest

---
+++ README.md
@@ -0,0 +1,9 @@
+# OLEDpaper
+
+Install non-reqfile dependencies:
+
+```
+git clone https://github.com/hmeine/qimage2ndarray
+cd qimage2ndarray
+python setup.py install
+```
--- gui.py
+++ gui.py
@@ -1,13 +1,16 @@
+from PyQt5 import QtCore
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QGraphicsScene, QGraphicsPixmapItem
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QFileDialog, 
+    QWidget, QHBoxLayout, QVBoxLayout, QLabel, QSlider)
 import sys
+import qimage2ndarray as QIA
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.current_file = None
-        self.button_func_dict = {"Load Image": self.load_image}
+        self.button_func_dict = {"Load Image": self._load_image}
 
         self.setWindowTitle("OLEDpaper")
         self.centralWidget = QWidget()
@@ -21,12 +24,19 @@ class MainWindow(QMainWindow):
             self.buttonsWidgetLayout.addWidget(button)
             button.clicked.connect(list(self.button_func_dict.values())[idx])
             # assign function to buttons, probably use dict where key=button name, value=button function
-        
-        self.mainCanvas = QWidget()
-        self.mainCanvas.setMinimumWidth(320)
-        self.mainCanvas.setMinimumHeight(480)
+
+        self.slider_r, self.slider_g, self.slider_b = [QSlider(self)]*3
+        for i in [self.slider_r, self.slider_g, self.slider_b]:
+            i.setOrientation(QtCore.Qt.Horizontal)
+            i.setMinimum(0)
+            i.setMaximum(255)
+            i.sliderMoved.connect(self._slider_moved)
+            self.vLayout.addWidget(i)
+
+        self.mainCanvas = QLabel()
+        # self.mainCanvas.setMinimumWidth(480)
+        # self.mainCanvas.setMinimumHeight(720)
         self.mainCanvas.setStyleSheet('* {background: gray;}')
-        self.label_image = QLabel(self.mainCanvas)
 
         self.vLayout.addWidget(self.mainCanvas)
         self.vLayout.addWidget(self.buttonsWidget)
@@ -35,16 +45,23 @@ class MainWindow(QMainWindow):
         # load_button.clicked.connect(self.load_image)
         # self.setCentralWidget(load_button)
     
-    def load_image(self):
+    def _load_image(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         fileName, _ = QFileDialog.getOpenFileName(self,"Select Image", "","All Files (*);;PNG Image (*.png);;JPG Image (*.jpg)", options=options)
         if fileName:
             self.current_file = fileName
-            scene = QGraphicsScene(self)
+            # label = QLabel(self)
             pixmap = QPixmap(fileName)
-            item = QGraphicsPixmapItem(pixmap)
-            self.label_image.setPixmap(pixmap)
+            self.mainCanvas.setPixmap(pixmap)
+            # self.vLayout.addWidget(label)
+            self.mainCanvas.setMinimumWidth(pixmap.width())
+            self.mainCanvas.setMinimumHeight(pixmap.height())
+            self.show()
+    
+    def _slider_moved(self, val):
+        pass
+
 
 app = QApplication(sys.argv)
 window = MainWindow()
